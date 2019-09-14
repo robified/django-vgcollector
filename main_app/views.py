52dec5bbeab6c40b33b557cb213b46198dd88009
Robin <daorob@gmail.com>
2019-09-14 14:59:13 -0500

    Complete home view fn.

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+# Add the follwoing import
+from django.http import HttpResponse
 
 # Create your views here.
+# Define the home view
+def home(request):
+    return HttpResponse('<h1>Herro! /ᐠ｡‸｡ᐟ\ﾉ Here\'s the video game collector homepage.</h1>')
