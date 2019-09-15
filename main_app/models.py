80af71b1f6a26272b5a8fd7f5bd3c15ebec1132d
Robin <daorob@gmail.com>
2019-09-15 01:03:34 -0500

    Create/Finish videogame model.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Videogame(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    description = models.TextField(max_length=350)
+    year = models.IntegerField()
+
+    # new code below
+    def __str__(self):
+        return self.name
