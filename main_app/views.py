6ec0a5cb4fd7ed2230ceb2501322694515345900
Robin <daorob@gmail.com>
2019-09-16 13:20:10 -0500

    Import login and UserCreationForm. Create signup fn.

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-# Add the following import
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-# Add the following import
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # from django.http import HttpResponse # Okay to delete this line because home is now rending a home.html
 from .models import Videogame, Console
 from .forms import PlaytimeForm
@@ -93,3 +93,21 @@ def add_playtime(request, videogame_id):
         new_feeding.videogame_id = videogame_id
         new_feeding.save()
     return redirect('detail', videogame_id=videogame_id)
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
