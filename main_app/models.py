328647d42769e7b0ad551a15c1a5d681947987fd
Robin <daorob@gmail.com>
2019-09-16 04:36:26 -0500

    Create Playtime model. Pass makemigration/migrate. Test model in shell working fine.

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,14 @@ from django.db import models
 # Import the reverse function
 from django.urls import reverse
 
+# A tuple of 2-tuples
+TIME_OF_DAY = (
+    ('M', 'Morning'),
+    ('A', 'Afternoon'),
+    ('E', 'Evening'),
+    ('N', 'Night')
+)
+
 # Create your models here.
 class Videogame(models.Model):
     name = models.CharField(max_length=100)
@@ -16,3 +24,20 @@ class Videogame(models.Model):
     # add this method
     def get_absolute_url(self):
         return reverse('detail', kwargs={'videogame_id': self.id})
+
+class Playtime(models.Model):
+    date = models.DateField()
+    time_of_day = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=TIME_OF_DAY,
+        # set the default value for meal to be 'B'
+        default=TIME_OF_DAY[3][0]
+    )
+
+    # Create a videogame_id FK
+    videogame = models.ForeignKey(Videogame, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_time_of_day_display()} on {self.date}"
