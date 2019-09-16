b52ed3a0209ce4c05a76a290c4a63037f3fb2652
Robin <daorob@gmail.com>
2019-09-16 12:00:47 -0500

    Add login redirect url when user successfully logs in.

--- vgcollector/settings.py
+++ vgcollector/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Add this variable to specify where successful logins should redirect to
+LOGIN_REDIRECT_URL = '/videogames/'
